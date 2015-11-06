bf2b5497ee9415e03d10c2ce3f0aeda0f01dfb99
Nick Matantsev <nick.m@myplanetdigital.com>
2015-11-05 19:28:18 -0500

    Added trigger message

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ var slack = new Slack(API_TOKEN, false, true);
 slack.on('open', function() {
     console.log("Connected to " + slack.team.name + " as @" + slack.self.name);
 
-    var channelList = Object.keys(slack.channels).map(function (k) { return slack.channels[k]; });
+    // var channelList = Object.keys(slack.channels).map(function (k) { return slack.channels[k]; });
 
-    channelList.filter(function (c) { return c.is_member; }).forEach(function (c) {
-        console.log('part of', c.name);
-    });
+    // channelList.filter(function (c) { return c.is_member; }).forEach(function (c) {
+    //     console.log('part of', c.name);
+    // });
 });
 
-// slack.on('message', function(message) {
-//     console.log(message);
-// });
+slack.on('message', function(message) {
+    if (message.user === slack.self.id) {
+        console.log('ignoring message from self');
+        return;
+    }
+
+    var messageMatch = message.text && /estimobot/.exec(message);
+
+    if (!messageMatch) {
+        console.log('ignoring non-trigger message');
+        return;
+    }
+
+    console.log(message);
+});
 
 slack.on('error', function(err) {
     console.error("Error", err);
