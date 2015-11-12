6ff5fe1c69447612a0fb7eda29247fa219ea3441
Nick Matantsev <nick.m@myplanetdigital.com>
2015-11-12 16:45:52 -0500

    Playing with routing

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 
-var RootRoute = require('atomic-routes');
+var RootState = require('atomic-routes');
 var boxbits = require('boxbits');
 
 var topBarLogo = require('./topBarLogo');
@@ -8,17 +8,26 @@ var hamburgerIcon = require('./hamburgerIcon');
 var server = require('__server')();
 
 boxbits.run(function (b) {
-    var rootRoute = new RootRoute();
+    var rootState = new RootState();
 
-    rootRoute.when('/avatar', function () {
-        console.log('avatar!');
-    });
+    var mainRoute = rootState.whenRoot();
+    var tokenRoute = rootState.when('/token');
 
     return function (screenW, screenH) {
         return [
             b.box(0, 0, 48, 48, b.image(32, 32, hamburgerIcon)),
 
-            b.box(48, 0, screenW - 48, 48, b.image(48, 48, topBarLogo))
+            b.box(48, 0, screenW - 48, 48, b.image(48, 48, topBarLogo)),
+
+            (mainRoute.getIsActive()
+                ? b.box(48, 96, screenW - 96, screenH - 96)
+                : null
+            ),
+
+            (tokenRoute.getIsActive()
+                ? b.box(0, 48, screenW, screenH)
+                : null
+            )
         ];
     };
 });
