f87bb37d791bf46bf686dcaf2431ed515a97b933
Nick Matantsev <nick.m@myplanetdigital.com>
2015-11-12 17:46:00 -0500

    Moving Slack token setup to frontend

--- client.js
+++ client.js
@@ -1,20 +1,39 @@
-
 var RootState = require('atomic-routes');
 var boxbits = require('boxbits');
 
 var topBarLogo = require('./topBarLogo');
 var hamburgerIcon = require('./hamburgerIcon');
 
-var server = require('__server')();
-
 boxbits.run(function (b) {
     var rootState = new RootState();
+
+    var server = require('__server')();
+
+    var isSetup = null;
+    server.getIsSetup().then(function (v) {
+        isSetup = !!v;
+    });
 
     var mainRoute = rootState.whenRoot();
     var tokenRoute = rootState.when('/token');
 
+    function renderNonSetupScreen() {
+        // not sure yet...
+        if (isSetup === null) {
+            return b.box(0, 48, b.screenWidth, b.screenHeight - 48,
+                b.text('Loading...')
+            );
+        }
+
+        // otherwise, a real false
+        return b.box(0, 48, b.screenWidth, b.screenHeight - 48,
+            b.box(0, 96, b.screenWidth, 64, b.text('Slack token not set up yet')),
+            b.box(0, 160, b.screenWidth, 64, b.button('Set Up Slack Token'))
+        );
+    }
+
     function renderTokenScreen() {
-        return b.box(0, 48, b.screenWidth, b.screenHeight);
+        return b.box(0, 48, b.screenWidth, b.screenHeight - 48);
     }
 
     return function () {
@@ -23,14 +42,16 @@ boxbits.run(function (b) {
 
             b.box(48, 0, b.screenWidth - 48, 48, b.image(48, 48, topBarLogo)),
 
-            (mainRoute.getIsActive()
-                ? b.box(48, 96, b.screenWidth - 96, b.screenHeight - 96)
-                : null
-            ),
+            (isSetup
+                ? [
+                    (mainRoute.getIsActive()
+                        ? b.box(48, 96, b.screenWidth - 96, b.screenHeight - 96)
+                        : null
+                    ),
 
-            (tokenRoute.getIsActive()
-                ? renderTokenScreen()
-                : null
+                    tokenRoute.getIsActive() ? renderTokenScreen() : null
+                ]
+                : renderNonSetupScreen()
             )
         ];
     };
--- index.js
+++ index.js
@@ -1,44 +1,36 @@
+var Promise = require('bluebird');
 var Slack = require('slack-client');
 
-var API_TOKEN = 'xoxb-13350508710-PztlziztMDo14titlgwl9fKU';
-var CHANNEL_NAME = 'estimation';
-
-var slack = new Slack(API_TOKEN, false, true);
-
-slack.on('open', function() {
-    console.log("Connected to " + slack.team.name + " as @" + slack.self.name);
-
-    // var channelList = Object.keys(slack.channels).map(function (k) { return slack.channels[k]; });
-
-    // channelList.filter(function (c) { return c.is_member; }).forEach(function (c) {
-    //     console.log('part of', c.name);
-    // });
-});
-
-slack.on('message', function(message) {
-    if (message.user === slack.self.id) {
-        console.log('ignoring message from self');
-        return;
-    }
-
-    var messageMatch = message.text && /estimobot/.exec(message);
-
-    if (!messageMatch) {
-        console.log('ignoring non-trigger message');
-        return;
-    }
-
-    console.log(message);
-});
-
-slack.on('error', function(err) {
-    console.error("Error", err);
-});
-
-slack.login();
+var slack = null;
 
 module.exports = {
-    getCurrentVote: function () {
-        return 'test vote!';
+    getIsSetup: function () {
+        return slack !== null;
+    },
+
+    applyToken: function (token) {
+        if (slack) {
+            throw new Error('already applied token');
+        }
+
+        return new Promise(function (resolve, reject) {
+            var conn = new Slack(token, false, true);
+
+            conn.on('open', function () {
+                console.log("Connected to " + conn.team.name + " as @" + conn.self.name);
+
+                slack = conn;
+
+                resolve();
+            });
+
+            conn.on('error', function (err) {
+                console.error("Error", err);
+
+                reject(err);
+            });
+
+            conn.login();
+        });
     }
 };
